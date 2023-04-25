73e65e57b219ff35dfd5a9df022660b9f3a34061
ptjgml <dudntjgml@gmail.com>
2023-04-25 17:53:26 +0900

    baekjoon

--- 16139_인간-컴퓨터 상호작용.py
+++ 16139_인간-컴퓨터 상호작용.py
@@ -35,6 +35,8 @@
 # $r_i$번째 문자 사이에
 # $\alpha_i$가 나타나는 횟수를 출력한다.
 
+
+# 50점 풀이
 import sys
 S = sys.stdin.readline()
 q = int(sys.stdin.readline())
@@ -45,20 +47,21 @@ for i in range(q):
 
 
 
+# 100점 풀이
+import sys
+input = sys.stdin.readline
 
+s = input().rstrip()
+arr = [[0]*26]
+arr[0][ord(s[0])-97] = 1
+for i in range(1,len(s)):
+    arr.append(arr[-1][:])
+    arr[i][ord(s[i])-97] += 1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for _ in range(int(input())):
+    c,start,end = list(input().split())
+    start,end = map(int,[start,end])
+    if start == 0:
+        print(arr[end][ord(c)-97])
+    else:
+        print(arr[end][ord(c)-97]-arr[start-1][ord(c)-97])
